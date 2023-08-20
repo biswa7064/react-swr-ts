cf17b3195d4d9f15aedb57af1c99a7ca98e56882
biswa7064 <biswaranjan16856@gmail.com>
2023-08-21 00:55:32 +0530

    fix: update logic

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -1,7 +1,7 @@
 import { UserType } from "@/types/userTypes"
 
 export const getAuthDetails = async (): Promise<UserType> => {
-  const localData = localStorage.getItem("user") as string
+  const localData = (await localStorage.getItem("user")) as string
   const response = JSON.parse(localData)
   if (!response) {
     throw new Error("No details found...")
